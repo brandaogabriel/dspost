db3252fc9f90e62cb4a6a6768d4039d66aba8fbf
brandaogabriel <gabriel.brandao@ccc.ufcg.edu.br>
2021-10-28 21:18:24 -0300

    initial commit

---
+++ src/test/kotlin/com/devgabriel/dspost/DspostApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.devgabriel.dspost
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DspostApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
