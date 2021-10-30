3bcae0f9a5c06da0ad9e4941c5e508fbb4f95b5f
brandaogabriel <gabriel.brandao@ccc.ufcg.edu.br>
2021-10-30 12:45:52 -0300

    add endpoint to get user by id

--- src/main/kotlin/com/devgabriel/dspost/controllers/UserController.kt
+++ src/main/kotlin/com/devgabriel/dspost/controllers/UserController.kt
@@ -4,6 +4,7 @@ import com.devgabriel.dspost.models.dtos.UserResponse
 import com.devgabriel.dspost.repositories.UserRepository
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -18,5 +19,14 @@ class UserController(
         val users = userRepository.findAll()
         val usersResponse = users.map { user -> UserResponse(user) }
         return ResponseEntity.ok(usersResponse)
+    }
+
+    @GetMapping("/{id}")
+    fun userById(@PathVariable id: String): ResponseEntity<UserResponse> {
+        val possibleUser = userRepository.findById(id)
+        if(possibleUser.isEmpty) return ResponseEntity.notFound().build()
+
+        val userResponse = UserResponse(possibleUser.get())
+        return ResponseEntity.ok(userResponse)
     }
 }
